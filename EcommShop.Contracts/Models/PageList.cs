e9cc54cd20157460eb6a5601810e04af403f02a8
tronghinh2809hui <tronghinh2809hui@gmail.com>
2021-12-17 11:56:22 +0700

    Add pageList Model

---
+++ EcommShop.Contracts/Models/PageList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommShop.Contracts.Models
+{
+    public class PageList<T>
+    {
+        public int MaxPage { get; set; }
+        public int CurrentPage { get; set; }
+        public List<T> ListItem { get; set; }
+    }
+}
