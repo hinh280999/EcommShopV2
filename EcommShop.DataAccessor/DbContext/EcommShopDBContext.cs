3108b28ca157866cf2229bd22c78f8e250953794
tronghinh2809hui <tronghinh2809hui@gmail.com>
2021-12-17 11:13:02 +0700

    Create EcommShopDbContext

---
+++ EcommShop.DataAccessor/DbContext/EcommShopDBContext.cs
@@ -0,0 +1,24 @@
+﻿using EcommShop.DataAccessor.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommShop.DataAccessor.DBContext
+{
+    public class EcommShopDBContext : DbContext
+    {
+        public EcommShopDBContext(DbContextOptions<EcommShopDBContext> options) : base(options){}
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<ProductRating> ProductRatings { get; set; }
+    }
+}
--- EcommShop.DataAccessor/Entities/User.cs
+++ EcommShop.DataAccessor/Entities/User.cs
@@ -2,7 +2,7 @@
 {
     public class User : BaseEntity
     {
-        public string FirstName { get; set }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
