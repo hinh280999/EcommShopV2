d31e9d15b65cf9644aa64b63464d1c3a59007e29
tronghinh2809hui <tronghinh2809hui@gmail.com>
2021-12-30 09:08:25 +0700

    refactor structure, add customersite, integration test project

---
+++ EcommShopVer2.CustomerSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EcommShopVer2.CustomerSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EcommShopVer2.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace EcommShopVer2.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
