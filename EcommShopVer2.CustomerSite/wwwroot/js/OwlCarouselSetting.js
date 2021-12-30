d7a7e37c9f3b378563e872dd2ebaa835d2f8526b
tronghinh2809hui <tronghinh2809hui@gmail.com>
2021-12-30 10:32:38 +0700

    Add OwlCarousel lib, add banner slider home page

---
+++ EcommShopVer2.CustomerSite/wwwroot/js/OwlCarouselSetting.js
@@ -0,0 +1,40 @@
+﻿//$("#productNewArrivals .owl-carousel").owlCarousel({
+//    loop: false,
+//    margin: 0,
+//    nav: false,
+//    dots: false,
+//    autoplayHoverPause: true,
+//    autoplayTimeout: 4000,
+//    autoplay: true,
+//    items: 4
+//});
+//$("#pNew-Previous").click(function () {
+//    console.log("prev");
+//    $("#productNewArrivals .owl-carousel").trigger('prev.owl.carousel');
+//});
+
+//$("#pNew-Next").click(function () {
+//    console.log("next");
+//    $("#productNewArrivals .owl-carousel").trigger('next.owl.carousel');
+//});
+// Banner Top
+$("#Banner-Slider .owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    dots: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 3000,
+    autoplay: true,
+    items: 1
+});
+// Detail img slider
+//$("#productImageSlide .owl-carousel").owlCarousel({
+//    loop: false,
+//    margin: 5,
+//    nav: false,
+//    dots: false,
+//    autoplayHoverPause: false,
+//    autoplay: false,
+//    items: 4
+//});
